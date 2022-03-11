9e07bc81992dbcd18b63052210b96614649c10f9
eloix2 <eloix2@gmail.com>
2022-03-11 15:34:26 +0100

    Librerias y permisos
    añadidos, quizas alguna sobra

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_map/flutter_map.dart';
 
 void main() {
   runApp(const MyApp());
