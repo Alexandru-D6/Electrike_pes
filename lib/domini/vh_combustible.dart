b870dfb73f752f1ad81155608de70bb3a83fb349
peilinni16 <95467823+peilinni16@users.noreply.github.com>
2022-03-17 22:37:17 +0100

    vehicles without getters and setters

--- lib/domini/vh_combustible.dart
+++ lib/domini/vh_combustible.dart
@@ -1,44 +1,35 @@
 import 'package:flutter_project/domini/vehicle.dart';
 
 class vh_combustible extends vehicle {
-  late double _capacitat_deposit;
+  late double capacitat_deposit;
 
-  late double _emissio_co2;
+  late double emissio_co2;
 
-  vh_combustible.complet (double capacitat_deposit, double emissio_co2, double potencia, double consum, String marca, String model) :
-  super.complet(potencia, consum, marca, model) {
-    this._capacitat_deposit = capacitat_deposit;
-    this._emissio_co2 = emissio_co2;
-  }
+  vh_combustible.complet (this.capacitat_deposit, this.emissio_co2, double potencia, double consum, String marca, String model)
+  : super.complet(potencia, consum, marca, model);
 
-  vh_combustible.only_comb(this._capacitat_deposit, this._emissio_co2): super.buit();
+  vh_combustible.only_comb(this.capacitat_deposit, this.emissio_co2): super.buit();
 
   // getter
-  double get capacitat_deposit => _capacitat_deposit;
-
-  double get emissio_co2 => _emissio_co2;
 
   // setters
-  set capacitat_deposit(double value) {
-    _capacitat_deposit = value;
-  }
 
-  set emissio_co2(double value) {
-    _emissio_co2 = value;
-  }
-
+  @override
   set potencia(double potencia) {
     super.potencia = potencia;
   }
 
+  @override
   set consum(double consum) {
     super.consum = consum;
   }
 
+  @override
   set marca(String marca) {
     super.marca = marca;
   }
 
+  @override
   set model(String model) {
     super.model = model;
   }
--- lib/domini/vh_electric.dart
+++ lib/domini/vh_electric.dart
@@ -1,51 +1,48 @@
 import 'package:flutter_project/domini/vehicle.dart';
 
 class vh_electric extends vehicle {
-  late double _capacitat_bateria;
-  late Set _endolls ;
+  late double capacitat_bateria;
+  late Set endolls ;
 
 
   vh_electric.buit() : super.buit();
-  vh_electric.complet(double capacitat_bateria, double potencia, double consum, String marca, String model) :
-        super.complet(potencia, consum, marca, model) {
-    this._capacitat_bateria = capacitat_bateria;
-  }
+  vh_electric.complet(this.capacitat_bateria, double potencia, double consum, String marca, String model)
+  : super.complet(potencia, consum, marca, model);
 
   // GETTERS
-  double get capacitat_bateria => _capacitat_bateria;
 
   // SETTERS
-  set capacitat_bateria(double value) {
-    _capacitat_bateria = value;
-  }
 
+  @override
   set potencia(double potencia) {
     super.potencia = potencia;
   }
 
+  @override
   set consum(double consum) {
     super.consum = consum;
   }
-
+  
+  @override
   set marca(String marca) {
     super.marca = marca;
   }
 
+  @override
   set model(String model) {
     super.model = model;
   }
 
   void afegir_endoll(String endoll) {
-    this._endolls.add(endoll);
+    endolls.add(endoll);
   }
 
   // BOOLEANS
-  /**
-   * @post: retorna cert si l'endoll es troba dins de la llista dels endolls que pot utilitzar el cotxe.
-   */
+  
+   /// @post: retorna cert si l'endoll es troba dins de la llista dels endolls que pot utilitzar el cotxe.
   bool cerca_endoll(String endoll) {
     bool trobat = false;
-    if (this._endolls.lookup(endoll)) trobat = true;
+    if (endolls.lookup(endoll)) trobat = true;
     return trobat;
   }
 }
