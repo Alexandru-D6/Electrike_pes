8fc3c3c2332d86127a847704323ed0c10bc12717
Alvarorodri <alvaro.rodriguez.rubio@estudiantat.upc.edu>
2022-03-17 23:35:58 +0100

    change ocupat to int

--- lib/domini/endoll.dart
+++ lib/domini/endoll.dart
@@ -3,12 +3,12 @@ import 'coordenada.dart';
 
 class Endoll{
   late int id;
-  late bool ocupat;
+  late int ocupat;
   late Coordenada coord;
   //late tipus_endoll tendoll;
   Endoll.origin(){
     id =-1;
-    ocupat = false;
+    ocupat = 0;
     coord =  Coordenada(0.0, 0.0);
   }
   Endoll(this.id, this.ocupat, this.coord);
