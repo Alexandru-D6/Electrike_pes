458c737cb672c34403795d8c7973be26e5fb3369
Alexandru Dumitru <alexandru666@outlook.es>
2022-03-18 11:39:57 +0100

    Revert "Merge branch 'Eloi' into Victor"
    This reverts commit 6e5f4402d8bab90eba853ee423f811ef85221880, reversing
    changes made to 1f6ced8ed4032373b3044eca0ae4159e43d5d734.

--- lib/interficie/page/login_page.dart
+++ lib/interficie/page/login_page.dart
@@ -14,13 +14,13 @@ class LogPage extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.teal,
+        color: Colors.white,
         child: Center(
           child: Column(
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Image.asset("../assets/logo.png"),
+              const FlutterLogo(size: 150),
               const SizedBox(height: 50),
               SignInButton(
                   buttonType: ButtonType.google,
