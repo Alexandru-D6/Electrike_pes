050864b6684d26138460d6814cb1e29630468840
peilinni16 <95467823+peilinni16@users.noreply.github.com>
2022-03-21 19:03:51 +0100

    service locator in progress

--- lib/domini/services/service_locator.dart
+++ lib/domini/services/service_locator.dart
@@ -2,17 +2,17 @@ import 'package:flutter_project/domini/services/google_login_adpt.dart';
 import 'package:flutter_project/domini/services/google_maps_adpt.dart';
 import 'package:get_it/get_it.dart';
 
-final ServiceLocator = GetIt.instance;
+final serviceLocator = GetIt.instance;
 
 void setUpLocator () {
-  ServiceLocator.registerLazySingleton<GoogleMapsAdpt>(() => (GoogleMapsAdpt()));
-  ServiceLocator.registerFactory<GoogleLoginAdpt>(() => GoogleLoginAdpt());
+  serviceLocator.registerLazySingleton<GoogleMapsAdpt>(() => (GoogleMapsAdpt()));
+  serviceLocator.registerFactory<GoogleLoginAdpt>(() => GoogleLoginAdpt());
 }
 
 GoogleMapsAdpt get getMapsService {
-  return ServiceLocator<GoogleMapsAdpt>();
+  return serviceLocator<GoogleMapsAdpt>();
 }
 
 GoogleLoginAdpt get getLoginService {
-  return ServiceLocator<GoogleLoginAdpt>();
+  return serviceLocator<GoogleLoginAdpt>();
 }
