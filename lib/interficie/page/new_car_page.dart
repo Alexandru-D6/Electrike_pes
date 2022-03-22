c5fd43ac036329ed3445f6ae5b7bb8418eb2f1d4
Víctor Asenjo <victorasenj@gmail.com>
2022-03-22 17:08:08 +0100

    suggester done

--- lib/interficie/page/new_car_page.dart
+++ lib/interficie/page/new_car_page.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
-import '../widget/form_input.dart';
+import '../widget/button_widget.dart';
+import '../widget/city_data.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
-
-class NewCarPage extends StatelessWidget {
+class NewCarPage extends StatefulWidget {
   const NewCarPage({Key? key}) : super(key: key);
+
+  @override
+  _NewCarPageState createState() => _NewCarPageState();
+}
+
+class _NewCarPageState extends State<NewCarPage> {
+  final formKey = GlobalKey<FormState>();
+  final controllerCity = TextEditingController();
+  final controllerFood = TextEditingController();
+
+  String? selectedCity;
+  String? selectedFood;
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -12,29 +25,60 @@ class NewCarPage extends StatelessWidget {
       title: const Text('New car'), //TODO: TRANSLATOR
       centerTitle: true,
       backgroundColor: Colors.red,
-
     ),
-    body:
-      Column(children: <Widget>[
-        FormInput("Name", "carName"),
-        FormInput("Car brand", "carBrand"),
-        FormInput("Car model", "carPenis"),
+    body: SafeArea(
+      child: Container(
+        padding: const EdgeInsets.all(24),
+        child: Form(
+          key: formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                buildCity(),
+                const SizedBox(height: 12),
+                buildSubmit(context)
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 
+  Widget buildCity() => TypeAheadFormField<String?>(
+    textFieldConfiguration: TextFieldConfiguration(
+      controller: controllerCity,
+      decoration: const InputDecoration(
+        labelText: 'City',
+        border: OutlineInputBorder(),
+      ),
+    ),
+    suggestionsCallback: CityData.getSuggestions,
+    itemBuilder: (context, String? suggestion) => ListTile(
+      title: Text(suggestion!),
+    ),
+    onSuggestionSelected: (String? suggestion) =>
+    controllerCity.text = suggestion!,
+    validator: (value) =>
+    value != null && value.isEmpty ? 'Please select a city' : null,
+    onSaved: (value) => selectedCity = value,
+  );
 
-        SizedBox(height: 10.0,),
-        TextButton(
-          child: Text('Add'), //todo: translator
-          onPressed: null,
-            /*
-            if (this._formKey.currentState.validate()) {
-              this._formKey.currentState.save();
-              Scaffold.of(context).showSnackBar(SnackBar(
-                  content: Text('Your Favorite City is ${this._selectedCity}')
-              ));
-            }
-            */
+  Widget buildSubmit(BuildContext context) => ButtonWidget(
+    text: 'Add', //todo: translator
+    onClicked: () {
+      final form = formKey.currentState!;
 
-        )
-      ])
+      if (form.validate()) {
+        form.save();
+
+        ScaffoldMessenger.of(context)
+          ..removeCurrentSnackBar()
+          ..showSnackBar(SnackBar(
+            content: Text(
+                'Your Favourite City is $selectedCity\nYour Favourite Food is $selectedFood'),
+          ));
+      }
+    }, icon: Icons.add_circle_rounded,
   );
 }
---
+++ lib/interficie/widget/city_data.dart
@@ -0,0 +1,79 @@
+class CityData {
+  //static final faker = Faker();
+
+  static final List<String> cities =
+  [
+    "Abarth",
+    "Alfa Romeo",
+    "Aston Martin",
+    "Audi",
+    "Bentley",
+    "BMW",
+    "Bugatti",
+    "Cadillac",
+    "Chevrolet",
+    "Chrysler",
+    "Citroën",
+    "Dacia",
+    "Daewoo",
+    "Daihatsu",
+    "Dodge",
+    "Donkervoort",
+    "DS",
+    "Ferrari",
+    "Fiat",
+    "Fisker",
+    "Ford",
+    "Honda",
+    "Hummer",
+    "Hyundai",
+    "Infiniti",
+    "Iveco",
+    "Jaguar",
+    "Jeep",
+    "Kia",
+    "KTM",
+    "Lada",
+    "Lamborghini",
+    "Lancia",
+    "Land Rover",
+    "Landwind",
+    "Lexus",
+    "Lotus",
+    "Maserati",
+    "Maybach",
+    "Mazda",
+    "McLaren",
+    "Mercedes-Benz",
+    "MG",
+    "Mini",
+    "Mitsubishi",
+    "Morgan",
+    "Nissan",
+    "Opel",
+    "Peugeot",
+    "Porsche",
+    "Renault",
+    "Rolls-Royce",
+    "Rover",
+    "Saab",
+    "Seat",
+    "Skoda",
+    "Smart",
+    "SsangYong",
+    "Subaru",
+    "Suzuki",
+    "Tesla",
+    "Toyota",
+    "Volkswagen",
+    "Volvo"
+  ];
+
+  static List<String> getSuggestions(String query) =>
+      List.of(cities).where((city) {
+        final cityLower = city.toLowerCase();
+        final queryLower = query.toLowerCase();
+
+        return cityLower.contains(queryLower);
+      }).toList();
+}
