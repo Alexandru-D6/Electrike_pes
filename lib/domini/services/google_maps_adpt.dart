77bd177a4c02d77e87e74e862985837aa22bea07
Alexandru Dumitru <alexandru666@outlook.es>
2022-03-25 10:48:56 +0100

    cleaning my branch

--- lib/domini/services/google_maps_adpt.dart
+++ lib/domini/services/google_maps_adpt.dart
@@ -1,4 +1,4 @@
-import 'package:geocoder2/geocoder2.dart';
+import 'package:geocode/geocode.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:tuple/tuple.dart';
 
