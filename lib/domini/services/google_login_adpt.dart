bc1c5bdf540c50bc87db49822cc052f9ebda7f76
Alexandru Dumitru <alexandru666@outlook.es>
2022-03-25 19:43:20 +0100

    WIP login

--- lib/domini/services/google_login_adpt.dart
+++ lib/domini/services/google_login_adpt.dart
@@ -1,9 +1,16 @@
 import 'package:google_sign_in/google_sign_in.dart';
+import '../../misc/user_preferences.dart';
 
 class GoogleLoginAdpt {
   static final _instance = GoogleLoginAdpt._internal();
   var googleMapApiKey = "AIzaSyBN9tjrv5YdkS1K-E1xP9UVLEkSnknU0yY";
   static final _googleSignIn = GoogleSignIn();
+
+  //User info
+  var _id;
+  var name;
+  var email;
+  var photoUrl;
 
   factory GoogleLoginAdpt() {
     return _instance;
@@ -11,7 +18,17 @@ class GoogleLoginAdpt {
 
   GoogleLoginAdpt._internal();
 
-  Future<GoogleSignInAccount?> login() => _googleSignIn.signIn();
+  Future<GoogleSignInAccount?> login() {
+      final user = _googleSignIn.signIn();
+      user.then((u) {
+        _id = u?.id;
+        name = u?.displayName;
+        email = u?.email;
+        photoUrl = u?.photoUrl;
+        print(u);
+      });
+      return user;
+  }
 
 
 }
