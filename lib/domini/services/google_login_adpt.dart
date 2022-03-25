5665ecc38f8a033392f6e933a4507fe6ef6d5168
Víctor Asenjo <victorasenj@gmail.com>
2022-03-26 00:52:52 +0100

    Update google_login_adpt.dart

--- lib/domini/services/google_login_adpt.dart
+++ lib/domini/services/google_login_adpt.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
 import 'package:flutter_project/interficie/ctrl_presentation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
