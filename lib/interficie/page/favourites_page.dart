d0440a19dba57ad2251c89952e6d47f76d138e52
Víctor Asenjo <victorasenj@gmail.com>
2022-03-28 01:06:45 +0200

    Update favourites_page.dart

--- lib/interficie/page/favourites_page.dart
+++ lib/interficie/page/favourites_page.dart
@@ -1,17 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/domini/coordenada.dart';
+import 'package:flutter_project/interficie/ctrl_presentation.dart';
 import 'package:flutter_project/interficie/widget/lateral_menu_widget.dart';
 
 import '../constants.dart';
 
-class FavouritesPage extends StatelessWidget {
+class FavouritesPage extends StatefulWidget {
   const FavouritesPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    drawer: const NavigationDrawerWidget(),
-    appBar: AppBar(
-      title: const Text("Favourites"),
-      backgroundColor: mPrimaryColor,
-    ),
-  );
+  State<FavouritesPage> createState() => _FavouritesPageState();
+}
+
+class _FavouritesPageState extends State<FavouritesPage> {
+  @override
+  Widget build(BuildContext context) {
+    CtrlPresentation ctrlPresentation = CtrlPresentation();
+    List<Coordenada> chargerPoints = ctrlPresentation.getChargePointList();
+    return Scaffold(
+      drawer: const NavigationDrawerWidget(),
+      appBar: AppBar(
+        title: const Text("Favourites"),
+        backgroundColor: mPrimaryColor,
+      ),
+      body: ListView.separated(
+        itemCount: chargerPoints.length,
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+        itemBuilder: (BuildContext context, int index) {
+          Coordenada word = chargerPoints[index];
+          bool isSaved = ctrlPresentation.favs.contains(word);
+
+          return ListTile(
+            title: Text(ctrlPresentation.getInfoCharger(word.latitud, word.longitud)[1]),
+            trailing: Icon(
+              isSaved ? Icons.favorite : Icons.favorite_border,
+              color: isSaved ? Colors.red : null,
+            ),
+            onTap: () {
+              setState(() {
+                if (isSaved) {
+                  ctrlPresentation.favs.remove(word);
+                } else {
+                  ctrlPresentation.favs.add(word);
+                }
+              });
+            },
+          );
+        },
+      ),
+    );
+  }
 }
