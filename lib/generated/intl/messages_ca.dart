3b8e55fcf10f53d29a882c5b44b8e177b737a9cf
peilinni16 <95467823+peilinni16@users.noreply.github.com>
2022-04-13 23:43:38 +0200

    tasca #167: ca arreglat

--- lib/generated/intl/messages_ca.dart
+++ lib/generated/intl/messages_ca.dart
@@ -20,6 +20,43 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'ca';
 
+  static String m0(howMany) =>
+      "${Intl.plural(howMany, one: 'You have 1 notification', other: 'You have ${howMany} notifications')}";
+
+  static String m1(name) => "Welcome ${name}";
+
+  static String m2(firstName, lastName) =>
+      "My name is ${lastName}, ${firstName} ${lastName}";
+
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{};
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "achievements": MessageLookupByLibrary.simpleMessage("Trofeus"),
+        "add": MessageLookupByLibrary.simpleMessage("Afegir"),
+        "alertSureDeleteCarContent": MessageLookupByLibrary.simpleMessage(
+            "Borrar un vehicle és una acció permanent que elimina totes les dades associades.\n Estas segur de voler continuar?\n"),
+        "alertSureDeleteCarTitle": MessageLookupByLibrary.simpleMessage(
+            "Estas segur de voler eliminar aquest vehicle?"),
+        "allFavourites":
+            MessageLookupByLibrary.simpleMessage("Els meus favorits"),
+        "bicing": MessageLookupByLibrary.simpleMessage("Bicing"),
+        "carBrand": MessageLookupByLibrary.simpleMessage("Marca del vehicle"),
+        "chargers": MessageLookupByLibrary.simpleMessage("Carregadors"),
+        "clickToLogin": MessageLookupByLibrary.simpleMessage("Pulsa per iniciar sessió"),
+        "contactUs": MessageLookupByLibrary.simpleMessage("Contacta'ns"),
+        "favourites": MessageLookupByLibrary.simpleMessage("Favorits"),
+        "garage": MessageLookupByLibrary.simpleMessage("Garatge"),
+        "howManyCars": m0,
+        "information": MessageLookupByLibrary.simpleMessage("Informació"),
+        "language": MessageLookupByLibrary.simpleMessage("Idioma"),
+        "login": MessageLookupByLibrary.simpleMessage("Iniciar sessió"),
+        "logout": MessageLookupByLibrary.simpleMessage("Tancar sessió"),
+        "map": MessageLookupByLibrary.simpleMessage("Mapa"),
+        "newCar": MessageLookupByLibrary.simpleMessage("Nou vehicle"),
+        "systemLanguage":
+            MessageLookupByLibrary.simpleMessage("Idioma del sistema"),
+        "textWithPlaceholder": m1,
+        "textWithPlaceholders": m2,
+        "toAddCarLogin": MessageLookupByLibrary.simpleMessage(
+            "Per afegir un vehicle has d'iniciar sessió")
+      };
 }
