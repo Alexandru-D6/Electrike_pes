2922155330db8da518103e322377882cb3609348
ferranjrr <ferrandelavargaantoja@gmail.com>
2022-04-24 21:31:34 +0200

    Proves
    Tester + classe notifications + install packeges & device config

---
+++ lib/domini/services/tester_notifications_ferran.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_project/domini/services/local_notifications_adpt.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+
+
+main() async {
+
+  model.sheduledNotification()
+
+}
