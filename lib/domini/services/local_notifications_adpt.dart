6f96f5758c48e66f14d53ce474dcacf296c6c4dd
peilinni16 <95467823+peilinni16@users.noreply.github.com>
2022-04-25 20:31:53 +0200

    some changes in ServiceLocator

--- lib/domini/services/local_notifications_adpt.dart
+++ lib/domini/services/local_notifications_adpt.dart
@@ -1,4 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_project/interficie/constants.dart';
+import 'package:flutter_project/interficie/ctrl_presentation.dart';
+import 'package:flutter_project/interficie/page/favourites_page.dart';
+import 'package:flutter_project/interficie/page/garage_page.dart';
+import 'package:flutter_project/interficie/widget/ocupation_chart.dart';
 
 class LocalNotificationAdpt {
   static final _instance = LocalNotificationAdpt._internal();
@@ -9,9 +16,8 @@ class LocalNotificationAdpt {
 
   LocalNotificationAdpt._internal();
 
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
   FlutterLocalNotificationsPlugin();
 
-  final AndroidInitializationSettings initializationSettingsAndroid =
-  const AndroidInitializationSettings('assets/images/logo.png');
 }
