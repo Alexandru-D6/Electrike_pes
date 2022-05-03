7fb32154fd17ef967828b6856d88d7dec8ec0c05
Alexandru Dumitru <alexandru666@outlook.es>
2022-05-03 15:39:22 +0200

    fix

--- lib/interficie/widget/google_map.dart
+++ lib/interficie/widget/google_map.dart
@@ -11,6 +11,7 @@ import 'package:flutter_project/interficie/ctrl_presentation.dart';
 import 'package:flutter_project/libraries/flutter_google_maps/src/core/markers_information.dart';
 import 'bicing_point_detail_info.dart';
 import 'charge_point_detail_info.dart';
+import 'info_ruta.dart';
 
 
 CtrlPresentation ctrlPresentation = CtrlPresentation();
@@ -188,6 +189,7 @@ class _MyMapState extends State<MyMap> {
             child: FloatingActionButton(
               onPressed: (){
                 ctrlPresentation.clearAllRoutes();
+                showInfoRuta(context);
                 ctrlPresentation.makeRoute();
               },
               heroTag: "Ruta",
@@ -337,6 +339,28 @@ class _MyMapState extends State<MyMap> {
       );
       }
     }
+  }
+
+  showInfoRuta(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        backgroundColor: cTransparent,
+        builder: (builder) {
+          return Stack(
+            children: [
+              Positioned(
+                left: 24,
+                right: 24,
+                bottom: 24,
+                child: Stack(
+                  children: const [
+                    InfoRuta(),
+                  ],
+                ),
+              ),
+            ],
+          );
+        });
   }
 }
 
