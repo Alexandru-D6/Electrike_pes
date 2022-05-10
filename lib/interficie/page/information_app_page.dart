528831a4f9681b934478198db8d429abb70b559b
Víctor Asenjo <victorasenj@gmail.com>
2022-05-10 12:28:18 +0200

    task #X information page

--- lib/interficie/page/information_app_page.dart
+++ lib/interficie/page/information_app_page.dart
@@ -1,110 +1,197 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/interficie/constants.dart';
+import 'package:flutter_project/interficie/ctrl_presentation.dart';
+import 'package:flutter_project/interficie/widget/google_map.dart';
 import 'package:flutter_project/interficie/widget/lateral_menu_widget.dart';
 
-class InformationAppPage extends StatefulWidget {
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+class InformationAppPage extends StatelessWidget {
   @override
-  _InformationAppPageState createState() => _InformationAppPageState();
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.transparent),
+    );
+
+    return MaterialApp(
+      title: 'Introduction screen',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: OnBoardingPage(),
+    );
+  }
 }
 
-class _InformationAppPageState extends State<InformationAppPage> {
-  List<Widget> slides = itemsInformationApp
-      .map((item) => Container(
-      padding: const EdgeInsets.symmetric(horizontal: 18.0),
-      child: Column(
-        children: <Widget>[
-          Flexible(
-            flex: 1,
-            fit: FlexFit.tight,
-            child: Image.asset(
-              item['image'],
-              fit: BoxFit.fitWidth,
-              width: 220.0,
-              alignment: Alignment.bottomCenter,
-            ),
-          ),
-          Flexible(
-            flex: 1,
-            fit: FlexFit.tight,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 30.0),
-              child: Column(
-                children: <Widget>[
-                  Text(item['header'],
-                      style: const TextStyle(
-                          fontSize: 50.0,
-                          fontWeight: FontWeight.w300,
-                          color: Color(0XFF3F3D56),
-                          height: 2.0)),
-                  Text(
-                    item['description'],
-                    style: const TextStyle(
-                        color: Colors.grey,
-                        letterSpacing: 1.2,
-                        fontSize: 16.0,
-                        height: 1.3),
-                    textAlign: TextAlign.center,
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      )))
-      .toList();
-
-  List<Widget> indicator() => List<Widget>.generate(
-      slides.length,
-          (index) => Container(
-        margin: const EdgeInsets.symmetric(horizontal: 3.0),
-        height: 10.0,
-        width: 10.0,
-        decoration: BoxDecoration(
-            color: currentPage.round() == index
-                ? const Color(0XFF256075)
-                : const Color(0XFF256075).withOpacity(0.2),
-            borderRadius: BorderRadius.circular(10.0)),
-      ));
-
-  double currentPage = 0.0;
-  final _pageViewController = PageController();
-
+class OnBoardingPage extends StatefulWidget {
   @override
-  void initState() {
-    super.initState();
-    _pageViewController.addListener(() {
-      setState(() {
-        currentPage = _pageViewController.page!;
-      });
-    });
+  _OnBoardingPageState createState() => _OnBoardingPageState();
+}
+
+class _OnBoardingPageState extends State<OnBoardingPage> {
+  final introKey = GlobalKey<IntroductionScreenState>();
+  CtrlPresentation ctrlPresentation = CtrlPresentation();
+  void _onIntroEnd(context) {
+    /*Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) => MyMap()),
+    );*/
+    ctrlPresentation.toMainPage(context);
+  }
+
+  Widget _buildFullscreenImage() {
+    return Image.asset(
+      'assets/brandCars/rayo.png',
+      fit: BoxFit.cover,
+      height: double.infinity,
+      width: double.infinity,
+      alignment: Alignment.center,
+    );
+  }
+
+  Widget _buildImage(String assetName, [double width = 350]) {
+    return Image.asset('assets/brandCars/$assetName', width: width);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          PageView.builder(
-            controller: _pageViewController,
-            itemCount: slides.length,
-            itemBuilder: (BuildContext context, int index) {
-              return slides[index];
-            },
+    const bodyStyle = TextStyle(fontSize: 19.0);
+
+    const pageDecoration = PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      key: introKey,
+      globalBackgroundColor: Colors.white,
+      globalHeader: Align(
+        alignment: Alignment.topRight,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16, right: 16),
+            child: _buildImage('audi.png', 100),
           ),
-          Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                margin: const EdgeInsets.only(top: 70.0),
-                padding: const EdgeInsets.symmetric(vertical: 40.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: indicator(),
-                ),
-              )
-            //  ),
-          )
-          // )
-        ],
+        ),
+      ),
+      globalFooter: SizedBox(
+        width: double.infinity,
+        height: 60,
+        child: ElevatedButton(
+          child: const Text(
+            'Let\'s go right away!',
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () => _onIntroEnd(context),
+        ),
+      ),
+      pages: [
+        PageViewModel(
+          title: "Fractional shares",
+          body:
+          "Instead of having to buy an entire share, invest any amount you want.",
+          image: _buildImage('rayo.png'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Learn as you go",
+          body:
+          "Download the Stockpile app and master the market with our mini-lesson.",
+          image: _buildImage('porsche.png'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Kids and teens",
+          body:
+          "Kids and teens can track their stocks 24/7 and place trades that you approve.",
+          image: _buildImage('seat.png'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Full Screen Page",
+          body:
+          "Pages can be full screen as well.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id euismod lectus, non tempor felis. Nam rutrum rhoncus est ac venenatis.",
+          image: _buildFullscreenImage(),
+          decoration: pageDecoration.copyWith(
+            contentMargin: const EdgeInsets.symmetric(horizontal: 16),
+            fullScreen: true,
+            bodyFlex: 2,
+            imageFlex: 3,
+          ),
+        ),
+        PageViewModel(
+          title: "Another title page",
+          body: "Another beautiful body text for this example onboarding",
+          image: _buildImage('tesla.png'),
+          footer: ElevatedButton(
+            onPressed: () {
+              introKey.currentState?.animateScroll(0);
+            },
+            child: const Text(
+              'FooButton',
+              style: TextStyle(color: Colors.white),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.lightBlue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+          ),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Title of last page - reversed",
+          bodyWidget: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("Click on ", style: bodyStyle),
+              Icon(Icons.edit),
+              Text(" to edit a post", style: bodyStyle),
+            ],
+          ),
+          decoration: pageDecoration.copyWith(
+            bodyFlex: 2,
+            imageFlex: 4,
+            bodyAlignment: Alignment.bottomCenter,
+            imageAlignment: Alignment.topCenter,
+          ),
+          image: _buildImage('mercedes.png'),
+          reverse: true,
+        ),
+      ],
+      onDone: () => _onIntroEnd(context),
+      //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      showSkipButton: false,
+      skipOrBackFlex: 0,
+      nextFlex: 0,
+      showBackButton: true,
+      //rtl: true, // Display as right-to-left
+      back: const Icon(Icons.arrow_back),
+      skip: const Text('Skip', style: TextStyle(fontWeight: FontWeight.w600)),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+      curve: Curves.fastLinearToSlowEaseIn,
+      controlsMargin: const EdgeInsets.all(16),
+      controlsPadding: kIsWeb
+          ? const EdgeInsets.all(12.0)
+          : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+      dotsContainerDecorator: const ShapeDecoration(
+        color: Colors.black87,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        ),
       ),
     );
   }
