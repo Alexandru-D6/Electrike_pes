e99bc948c6a4bc1b13b114e2330e1c63dfa82b76
eloix2 <eloix2@gmail.com>
2022-05-15 20:26:58 +0200

    task #234 canviat a stateful, dropdown button per canviar el dia

--- lib/interficie/page/chart_page.dart
+++ lib/interficie/page/chart_page.dart
@@ -2,20 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_project/domini/data_graphic.dart';
 import 'package:flutter_project/interficie/constants.dart';
 import 'package:flutter_project/interficie/ctrl_presentation.dart';
+import 'package:flutter_project/interficie/widget/day_of_week_picker_widget.dart';
 import 'package:flutter_project/interficie/widget/ocupation_chart.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 
 
 
-class ChartPage extends StatelessWidget {
+class ChartPage extends StatefulWidget {
   const ChartPage({Key? key}) : super(key: key);
 
+  @override
+  State<ChartPage> createState() => _ChartPageState();
+}
+  class _ChartPageState extends State<ChartPage> {
+    String dropdownValue = 'Monday'; //todo: añadir dropdown
   @override
   Widget build(BuildContext context) {
     final pointTitle = ModalRoute.of(context)!.settings.arguments as String;
     //CtrlPresentation ctrlPresentation = CtrlPresentation();
     return Scaffold(
-      backgroundColor: mPrimaryColor,
+      backgroundColor: Colors.white,
       appBar: buildAppBar(context),
       body:
       Column(
@@ -26,12 +32,13 @@ class ChartPage extends StatelessWidget {
               RichText(
                 text: TextSpan(
                   text: pointTitle,
-                  style: const TextStyle(color: Colors.white, fontSize: 25),
+                  style: const TextStyle(color: Colors.black, fontSize: 25),
 
 
               )
             ),
           ),
+          DayOfWeekPickerWidget(),
           Container(
             padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
             alignment: Alignment.bottomCenter,
@@ -55,7 +62,7 @@ class ChartPage extends StatelessWidget {
     return [
       charts.Series<DataGraphic, String>(
           id: 'Ocupacio',
-          colorFn: (_, __) => charts.MaterialPalette.purple.shadeDefault,
+          colorFn: (_, __) => charts.MaterialPalette.green.shadeDefault,
           domainFn: (DataGraphic occupation, _) => occupation.hour.toInt().toString(),
           measureFn: (DataGraphic occupation, _) => occupation.percentage.round(),
           data: data,
@@ -72,4 +79,5 @@ class ChartPage extends StatelessWidget {
       title: const Text('Charts'),
     );
   }
+
 }
