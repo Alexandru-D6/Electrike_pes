f0ee0187b892dc2d9d9f2c55cc14a39eded46c63
eloix2 <eloix2@gmail.com>
2022-05-17 15:56:24 +0200

    por hacer: cambiar funcion getdistduration para que cargue la ruta en presentacion

--- lib/interficie/widget/custom_radio_button.dart
+++ lib/interficie/widget/custom_radio_button.dart
@@ -16,6 +16,7 @@ class _CustomRadioState extends State<CustomRadio> {
         setState(() {
           ctrlPresentation.routeType = index;
         });
+        if(index == 1) ctrlPresentation.makeRoute();
       },
       child: Text(
         text,
