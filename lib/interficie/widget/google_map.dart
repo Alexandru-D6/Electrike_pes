9c257c679dad74de3155187aaefb8861b19130c9
Alexandru Dumitru <alexandru666@outlook.es>
2022-05-19 15:10:30 +0200

    fix merge

--- lib/interficie/widget/google_map.dart
+++ lib/interficie/widget/google_map.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:animated_floating_buttons/animated_floating_buttons.dart';
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
