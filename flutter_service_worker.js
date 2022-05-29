29cba348a09b7651e66cff73c060ee051895b850
Alexandru-D6 <Alexandru-D6@users.noreply.github.com>
2022-05-29 23:36:53 +0000

    Deploying to gh-pages from main @ f06fefb88f84963fa5ace5e5a0f34fec97922258 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -178,9 +178,9 @@ const RESOURCES = {
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "ec370b85c373b670e8eb1094cc3ab394",
-"index.html": "2d0f24ec85c287f94ea65d9a0f01765b",
-"/": "2d0f24ec85c287f94ea65d9a0f01765b"
+"main.dart.js": "a929ae41cd66ea82e20625498fa27382",
+"index.html": "dd6cda1e9ae64eeffd83d62b0c7a38eb",
+"/": "dd6cda1e9ae64eeffd83d62b0c7a38eb"
 };
 
 // The application shell files that are downloaded before a service worker can
