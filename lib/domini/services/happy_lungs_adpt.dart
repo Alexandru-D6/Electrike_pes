bf6f0c9df438be4ed38124becdf247ff3dc78db6
Alexandru Dumitru <alexandru666@outlook.es>
2022-05-31 15:10:58 +0200

    adding proxy to happyLungs

--- lib/domini/services/happy_lungs_adpt.dart
+++ lib/domini/services/happy_lungs_adpt.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:google_directions_api/google_directions_api.dart';
 
 class HappyLungsAdpt {
   final urlorg = 'http://ec2-18-208-246-30.compute-1.amazonaws.com:7000/v1/contamination/';
+  final urlorgWeb = 'https://obscure-lake-86305.herokuapp.com/http://ec2-18-208-246-30.compute-1.amazonaws.com:7000/v1/contamination/';
   final radius = 20000; //metres
 
   HappyLungsAdpt();
@@ -12,6 +14,8 @@ class HappyLungsAdpt {
   Future<Map<double, GeoCoord>> getEcoPoints(GeoCoord geoCoord) async{
     Map<double, GeoCoord> result = {};
     var urlc = urlorg+ geoCoord.latitude.toString() + '/' + geoCoord.longitude.toString() + '/' + radius.toString();
+    if (kIsWeb) urlc = urlorgWeb+ geoCoord.latitude.toString() + '/' + geoCoord.longitude.toString() + '/' + radius.toString();
+    
     var aux = (await http.get(Uri.parse(urlc)));
     var respUrl = jsonDecode(aux.body);
     for (var coord in respUrl) {
