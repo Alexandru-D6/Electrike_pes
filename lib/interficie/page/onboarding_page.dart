9a2a5599ddfaa447e11cf8582e70853b5a815cda
Alexandru Dumitru <alexandru666@outlook.es>
2022-05-31 20:53:39 +0200

    fixing bug of random routes

--- lib/interficie/page/onboarding_page.dart
+++ lib/interficie/page/onboarding_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project/domini/main_peilin.dart';
+import 'package:flutter_project/main.dart';
 import 'package:flutter_project/interficie/ctrl_presentation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
